28dafdfcdd3cd1b76a62c9e48b1ad2f223faecaa
NathiyaGopalakrishnan <50880726+NathiyaGopalakrishnan@users.noreply.github.com>
2019-05-31 20:44:11 +0530

    Create Vow.py

---
+++ Vow.py
@@ -0,0 +1,8 @@
+v=input()
+if(v=="a" or v=="e" or v=="i" or v=="o" or v=="u" or v=="A" or v=="E" or v=="I" or v=="O" or v=="U"):
+  print("Vowel")
+
+elif(v.isdigit()):
+  print("Invalid")
+else:
+  print("Consonant")
